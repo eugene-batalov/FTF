d7357101ea8b3e0f7ea83faaa7a8de8d5709fe01
batalov <batalov@inpolus.ru>
2016-01-10 22:46:22 +0300

    реклама

--- FTF_UnityProject/Assets/Scripts/Level1Manager.cs
+++ FTF_UnityProject/Assets/Scripts/Level1Manager.cs
@@ -4,166 +4,195 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.Linq;
+using GoogleMobileAds.Api;
 
 public class Level1Manager : MonoBehaviour
 {
-	public static Level1Manager Instance;
-	public static Action<GameObject> ActiveObjectPointed;
-	public static Action<GameObject> CorrectKuvshinkaPointed;
-	public static Action NextTurn;
-	public static bool Idle;
-	public static bool LevelDown;
+    public static Level1Manager Instance;
+    public static Action<GameObject> ActiveObjectPointed;
+    public static Action<GameObject> CorrectKuvshinkaPointed;
+    public static Action NextTurn;
+    public static bool Idle;
+    public static bool LevelDown;
 
-	public GameObject[] digits;
-	public AudioSource Haha1;
-	
-	public Text LevelText;
-	public Text LevelDownText;
+    public GameObject[] digits;
+    public AudioSource Haha1;
 
-	public Text RightWayText;
-	public Text IdleText;
-	public Text CurrentJumpsBeforeNextLevelText;
-	public Text CurrentCorrectJumpsSerieText;
+    public Text LevelText;
+    public Text LevelDownText;
 
-	public int CorrectJumpsBeforeNextLevel = 10;
-	public int CurrentJumpsBeforeNextLevel;
-	public int CurrentCorrectJumpsSerie;
-	public int Level;
+    public Text RightWayText;
+    public Text IdleText;
+    public Text CurrentJumpsBeforeNextLevelText;
+    public Text CurrentCorrectJumpsSerieText;
 
-	ArrayList _rightWay;
-	Dictionary<string, SpriteRenderer> _strelki;
-	GameObject[] _kuvshinki;
-	bool _showRightWay;
-	bool _wasMistake;
+    public float AdsTimeout = 30f;
+    public int CorrectJumpsBeforeNextLevel = 10;
+    public int CurrentJumpsBeforeNextLevel;
+    public int CurrentCorrectJumpsSerie;
+    public int Level;
 
-	public void ShowRightWayOn ()
-	{
-		_showRightWay = true;
-	}
+    ArrayList _rightWay;
+    Dictionary<string, SpriteRenderer> _strelki;
+    GameObject[] _kuvshinki;
+    bool _showRightWay;
+    bool _wasMistake;
+    private InterstitialAd _interstitial;
+    private AdRequest _request;
 
-	void Awake ()
-	{
-		Instance = this;
-		_rightWay = new ArrayList ();
-		ActiveObjectPointed += RightOrWrongKuvshinkaPointed;
-		CurrentJumpsBeforeNextLevel = CorrectJumpsBeforeNextLevel;
-		CurrentCorrectJumpsSerie = 0;
-		Init ();
-		Level = 1;
-		_showRightWay = false;
-		_wasMistake = false;
-		foreach (var digit in digits) {
-			digit.SetActive (false);
-		}
-	}
+    public void ShowRightWayOn()
+    {
+        _showRightWay = true;
+    }
 
-	void Init ()
-	{
-		_strelki = new Dictionary<string, SpriteRenderer> ();
-		var strelkiArray = GameObject.FindGameObjectsWithTag ("strelka");
-		_kuvshinki = GameObject.FindGameObjectsWithTag ("kuvshinka");
-//		Debug.Log(_kuvshinki.Length + " kuvshinok added");
+    private void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        var adUnitId = "ca-app-pub-1989038349842297/1295232363";
+#elif UNITY_IPHONE
+        string adUnitId = "INSERT_IOS_INTERSTITIAL_AD_UNIT_ID_HERE";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        // Initialize an InterstitialAd.
+        _interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        _request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        _interstitial.LoadAd(_request);
+        _timeOut = false;
+    }
 
-		foreach (var strelka in strelkiArray) {
-//            Debug.Log("key: " + strelka.name + " added");
-			_strelki.Add (strelka.name, strelka.GetComponent<SpriteRenderer> ());
-		}
-	}
+    void Awake()
+    {
+        Instance = this;
+        _rightWay = new ArrayList();
+        ActiveObjectPointed += RightOrWrongKuvshinkaPointed;
+        CurrentJumpsBeforeNextLevel = CorrectJumpsBeforeNextLevel;
+        CurrentCorrectJumpsSerie = 0;
+        Init();
+        Level = 1;
+        _showRightWay = false;
+        _wasMistake = false;
+        foreach (var digit in digits)
+        {
+            digit.SetActive(false);
+        }
+        RequestInterstitial();
+    }
 
-	void Update ()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape)) 
-			Application.Quit(); 
-		LevelText.text = "Level " + Level;
-		//	RightWayText.text = "Right Way: ";
-		for (var i = _rightWay.Count - 1; i > -1; i--) {
-			//        RightWayText.text += ((GameObject) _rightWay[i]).transform.parent.parent.name.Replace("kyvshinka", "") +
-			//                             ((i == 0) ? "" : ", ");
-			if (i <= 0)
-				continue;
-			var from = ((GameObject)_rightWay [i]).transform.parent.parent.name.Replace ("kyvshinka", "");
-			var to = ((GameObject)_rightWay [i - 1]).transform.parent.parent.name.Replace ("kyvshinka", "");
-			_strelki [@from + "-" + to].enabled = _showRightWay;
-			digits [_rightWay.Count - 1 - i].GetComponent<SetUiTextPosition> ().go = ((GameObject)_rightWay [i - 1]).transform.parent.parent.gameObject;
-			digits [_rightWay.Count - 1 - i].SetActive (_showRightWay);
-		}
-//	    IdleText.text = "Idle: " + Idle;
-//		LevelDownText.text = "LevelDown: " + LevelDown;
-//		CurrentJumpsBeforeNextLevelText.text = "CurrentJumpsBeforeNextLevel: " + CurrentJumpsBeforeNextLevel;
-//		CurrentCorrectJumpsSerieText.text = "CurrentCorrectJumpsSerie: " + CurrentCorrectJumpsSerie;
-	}
+    void Init()
+    {
+        _strelki = new Dictionary<string, SpriteRenderer>();
+        var strelkiArray = GameObject.FindGameObjectsWithTag("strelka");
+        _kuvshinki = GameObject.FindGameObjectsWithTag("kuvshinka");
+        Debug.LogWarning(string.Format("Level1Manager Init: {0} kuvshinok added", _kuvshinki.Length));
 
-	public void LevelUp ()
-	{
-		Level++;
-		CurrentJumpsBeforeNextLevel = CorrectJumpsBeforeNextLevel;
-		CurrentCorrectJumpsSerie = 0;
-		NextTurn ();
-	}
+        foreach (var strelka in strelkiArray)
+        {
+            //            Debug.Log("key: " + strelka.name + " added");
+            _strelki.Add(strelka.name, strelka.GetComponent<SpriteRenderer>());
+        }
+    }
 
-	void Correct (GameObject go)
-	{
-		CorrectKuvshinkaPointed (go);
-		_rightWay.RemoveAt (_rightWay.Count - 1);
-		CurrentJumpsBeforeNextLevel--;
-		CurrentCorrectJumpsSerie++;
-		_wasMistake = false;
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        LevelText.text = "Level " + Level;
+        //	RightWayText.text = "Right Way: ";
+        for (var i = _rightWay.Count - 1; i > -1; i--)
+        {
+            //        RightWayText.text += ((GameObject) _rightWay[i]).transform.parent.parent.name.Replace("kyvshinka", "") +
+            //                             ((i == 0) ? "" : ", ");
+            if (i <= 0)
+                continue;
+            var from = ((GameObject)_rightWay[i]).transform.parent.parent.name.Replace("kyvshinka", "");
+            var to = ((GameObject)_rightWay[i - 1]).transform.parent.parent.name.Replace("kyvshinka", "");
+            _strelki[@from + "-" + to].enabled = _showRightWay;
+//            digits[_rightWay.Count - 1 - i].GetComponent<SetUiTextPosition>().go = ((GameObject)_rightWay[i - 1]).transform.parent.parent.gameObject;
+//            digits[_rightWay.Count - 1 - i].SetActive(_showRightWay);
+        }
+    }
 
-	void Wrong ()
-	{
-		if (_wasMistake)
-			_showRightWay = true;
-		if (LevelDownText.gameObject.activeSelf)
-			return;
-		// ошибка после понижения уровня не в счет
-		_wasMistake = true;
-		//				Debug.Log("MISTAKE!");
-		if (Haha1 != null && SoundsOnOff.Instance.SoundsOn == 1)
-			Haha1.Play ();
-		if (CurrentCorrectJumpsSerie < 5 && _rightWay.Count > 3) {
-			LevelDownText.gameObject.SetActive (true);
-			_showRightWay = true;
-			Level--;
-		}
-		CurrentCorrectJumpsSerie /= 2;
-	}
+    public void LevelUp()
+    {
+        Level++;
+        CurrentJumpsBeforeNextLevel = CorrectJumpsBeforeNextLevel;
+        CurrentCorrectJumpsSerie = 0;
+        NextTurn();
+    }
 
-	void RightOrWrongKuvshinkaPointed (GameObject go)
-	{
-		if (!Idle) return;
+    void Correct(GameObject go)
+    {
+ //       Debug.Log("Jump4Frog Level1Manager Correct");
+        CorrectKuvshinkaPointed(go);
+        _rightWay.RemoveAt(_rightWay.Count - 1);
+        CurrentJumpsBeforeNextLevel--;
+        CurrentCorrectJumpsSerie++;
+        _wasMistake = false;
+    }
 
-		_showRightWay = false;
+    private bool _timeOut;
+    IEnumerator ShowAds()
+    {
+        if(_timeOut) yield break;
+        _timeOut = true;
+        if (_interstitial.IsLoaded()) _interstitial.Show();
+        _interstitial.LoadAd(_request);
+        yield return new WaitForSeconds(AdsTimeout);
+        _timeOut = false;
+    }
 
-		Update ();
+    void Wrong()
+    {
+//        Debug.Log("Jump4Frog Level1Manager Wrong");
+        if (_wasMistake)
+            _showRightWay = true;
+        // ошибка после понижения уровня не в счет
+        if (LevelDownText.gameObject.activeSelf)
+            return;
 
-		if (go == (GameObject)_rightWay [0] || go == (GameObject)_rightWay [_rightWay.Count - 1]) //	Frog pointed
-			return;
-		
-		if (go == (GameObject)_rightWay [_rightWay.Count - 2]) Correct (go);
-		else Wrong ();
-	}
+        _wasMistake = true;
+        if (Haha1 != null && SoundsOnOff.Instance.SoundsOn == 1)
+            Haha1.Play();
+        if (CurrentCorrectJumpsSerie < 5 && _rightWay.Count > 3)
+        {
+            LevelDownText.gameObject.SetActive(true);
+            _showRightWay = true;
+            Level--;
+        }
+        CurrentCorrectJumpsSerie /= 2;
+        StartCoroutine(ShowAds());
+    }
 
-	public void SetMyFrogPosition (GameObject go)
-	{
-		_rightWay.Add (go);
-	}
+    void RightOrWrongKuvshinkaPointed(GameObject go)
+    {
+        if (!Idle) return;
+        if (go == (GameObject)_rightWay[0] || go == (GameObject)_rightWay[_rightWay.Count - 1]) //	Frog pointed
+            return;
+        _showRightWay = false;
+        Update();
+        if (go == (GameObject)_rightWay[_rightWay.Count - 2]) Correct(go);
+        else Wrong();
+    }
 
-	public void SetLeadingFrogPosition (GameObject go)
-	{
-		if (_rightWay.Count < 1)
-			_rightWay.Add (go);
-		else
-			_rightWay.Insert (0, go);
-	}
+    public void SetMyFrogPosition(GameObject go)
+    {
+        _rightWay.Add(go);
+    }
 
-	public GameObject GetFreeKuvshinka ()
-	{
-		var freePlaces = _kuvshinki.Except(_rightWay.ToArray()).ToArray();
-//		GameObject k;
-//		do {
-//			k = _kuvshinki [UnityEngine.Random.Range (0, _kuvshinki.Length)]; // выбрать случайную из списка кувшинок
-//		} while(_rightWay.Contains (k)); // если 
-		return (GameObject) freePlaces[UnityEngine.Random.Range (0, freePlaces.Length)];
-	}
+    public void SetLeadingFrogPosition(GameObject go)
+    {
+        if (_rightWay.Count < 1)
+            _rightWay.Add(go);
+        else
+            _rightWay.Insert(0, go);
+    }
+
+    public GameObject GetFreeKuvshinka()
+    {
+        var freePlaces = _kuvshinki.Except(_rightWay.ToArray()).ToArray();
+        return (GameObject)freePlaces[UnityEngine.Random.Range(0, freePlaces.Length)];
+    }
 }
--- FTF_UnityProject/Assets/Scripts/PointerDownReceiver.cs
+++ FTF_UnityProject/Assets/Scripts/PointerDownReceiver.cs
@@ -2,19 +2,12 @@
 using System.Collections;
 
 public class PointerDownReceiver : MonoBehaviour {
-	public GameObject GameObjectToSend;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public GameObject GameObjectToSend;
 
 	public void PointerDownReceived()
 	{
+        Debug.Log("Jump4Frog PointerDownReceiver PointerDownReceived");
 		if(GameObjectToSend == null) GameObjectToSend = gameObject;
 		if(Level1Manager.ActiveObjectPointed != null) Level1Manager.ActiveObjectPointed(GameObjectToSend);
 	}
