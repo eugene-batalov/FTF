f2d4ebe22e3d52bfbbc9cb4b94aa2bae9f680a8d
batalov <batalov@inpolus.ru>
2016-09-27 08:09:20 +0300

    кнопка Старт

--- FTF_UnityProject/Assets/Scripts/FrogController.cs
+++ FTF_UnityProject/Assets/Scripts/FrogController.cs
@@ -1,94 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FrogController : MonoBehaviour {
-	public Transform RotationPoint;
-	public float RotationSpeed; 
-	public Animator FrogAnimator;
-	public AudioSource FrogAudioSource;
-	public bool LeadingFrog;
+public class FrogController : MonoBehaviour
+{
+    public Transform RotationPoint;
+    public float RotationSpeed;
+    public Animator FrogAnimator;
+    public AudioSource FrogAudioSource;
+    public bool LeadingFrog;
 
-	IEnumerator Start () {
-		if(!LeadingFrog)
-		{
-			Level1Manager.CorrectKuvshinkaPointed += NextJump;
-			while(Level1Manager.Instance == null) yield return new WaitForEndOfFrame();
-			GameObject go = Level1Manager.Instance.GetFreeKuvshinka();
-			transform.SetParent(go.transform);
-			transform.localPosition = Vector3.zero;
-			Level1Manager.Instance.SetMyFrogPosition(go);
-			while(Level1Manager.NextTurn == null) yield return new WaitForEndOfFrame();
-			Level1Manager.NextTurn();
-		}
-		else 
-		{
-			Level1Manager.Idle = true;
-			Level1Manager.NextTurn += ()=> StartCoroutine(WaitAndStartNextTurn());
-			while(Level1Manager.Instance == null) yield return new WaitForEndOfFrame();
-			GameObject go = Level1Manager.Instance.GetFreeKuvshinka();
-			transform.SetParent(go.transform);
-			transform.localPosition = Vector3.zero;
-			Level1Manager.Instance.SetLeadingFrogPosition(go);
-		}
-	}
+    IEnumerator Start()
+    {
+        if (LeadingFrog)
+        {
+            Level1Manager.Idle = true;
+            Level1Manager.NextTurn += () => StartCoroutine(WaitAndStartNextTurn());
+            while (Level1Manager.Instance == null) yield return new WaitForEndOfFrame();
+            GameObject go = Level1Manager.Instance.GetFreeKuvshinka();
+            transform.SetParent(go.transform);
+            transform.localPosition = Vector3.zero;
+            Level1Manager.Instance.SetLeadingFrogPosition(go);
+        }
+        else
+        {
+            Level1Manager.CorrectKuvshinkaPointed += NextJump;
+            while (Level1Manager.Instance == null) yield return new WaitForEndOfFrame();
+            GameObject go = Level1Manager.Instance.GetFreeKuvshinka();
+            transform.SetParent(go.transform);
+            transform.localPosition = Vector3.zero;
+            Level1Manager.Instance.SetMyFrogPosition(go);
+            while (Level1Manager.NextTurn == null) yield return new WaitForEndOfFrame();
+            Level1Manager.NextTurn();
+        }
+    }
 
-	IEnumerator WaitAndStartNextTurn()
-	{
-		Level1Manager.Idle = false;
-		yield return new WaitForEndOfFrame();
-//		yield return new WaitForSeconds(0.1f * Level1Manager.Instance.CurrentJumpsBeforeNextLevel);
-		GameObject k = Level1Manager.Instance.GetFreeKuvshinka();
-		Level1Manager.Instance.SetLeadingFrogPosition(k);
-		NextJump(k);
-	}
+    IEnumerator WaitAndStartNextTurn()
+    {
+        Level1Manager.Idle = false;
+        yield return new WaitForEndOfFrame();
+        //		yield return new WaitForSeconds(0.1f * Level1Manager.Instance.CurrentJumpsBeforeNextLevel);
+        GameObject k = Level1Manager.Instance.GetFreeKuvshinka();
+        Level1Manager.Instance.SetLeadingFrogPosition(k);
+        NextJump(k);
+    }
 
-	public void NextJump(GameObject go)
-	{
-		if(FrogAudioSource != null && SoundsOnOff.Instance.SoundsOn == 1) FrogAudioSource.Play();
-		StartCoroutine(RotateAndJump(go));
-	}
-	
-	IEnumerator RotateAndJump(GameObject go)
-	{
-		Level1Manager.Idle = true; // можно делать следующий ход
-		var target = go.transform.position - RotationPoint.transform.position;
-		var angle = Vector3.Angle(transform.up, target);
-		if(FrogAnimator != null) FrogAnimator.Play("Rotate");
+    public void NextJump(GameObject go)
+    {
+        if (FrogAudioSource != null && SoundsOnOff.Instance.SoundsOn == 1) FrogAudioSource.Play();
+        StartCoroutine(RotateAndJump(go));
+    }
 
-		yield return new WaitForSeconds(0.1f);
-		if(RotationSpeed < 0.1) RotationSpeed = 1f;
+    IEnumerator RotateAndJump(GameObject go)
+    {
+        Level1Manager.Idle = true; // можно делать следующий ход
+        var target = go.transform.position - RotationPoint.transform.position;
+        var angle = Vector3.Angle(transform.up, target);
+        if (FrogAnimator != null) FrogAnimator.Play("Rotate");
 
-//		Debug.Log(string.Format("go:{0}, target: {1}, t/15: {2}", go.transform.parent.name, target, target/15f));
+        yield return new WaitForSeconds(0.1f);
+        if (RotationSpeed < 0.1) RotationSpeed = 1f;
 
-		for(var i=0; i< (int) (angle / RotationSpeed); i++)
-		{
-			transform.RotateAround(RotationPoint.position, Vector3.Cross(transform.up, target), RotationSpeed);// вескторное произведение чтобы определить направление поворота
-			yield return new WaitForSeconds(1f/30f); // частота кадров анимации поворота
-		}
-		if(FrogAnimator != null) FrogAnimator.Play("Jump");
-		for(var i=0; i<15; i++) // количество кадров анимации прыжка
-		{
-			transform.Translate(target/Mathf.Pow(2.005f, i+1f), Space.World);
-			yield return new WaitForSeconds(1f/30f); // частота кадров анимации прыжка
-		}
-		transform.SetParent(go.transform);
-		transform.localPosition = Vector3.zero;
-		if(!LeadingFrog) 
-		{
-			if(Level1Manager.Instance.LevelDownText.gameObject.activeSelf)
-			{
-				Level1Manager.Instance.LevelDownText.gameObject.SetActive(false);
-				yield break;
-			}
-			if(Level1Manager.NextTurn != null)Level1Manager.NextTurn();
-		}
-		else
-		{
-			if(Level1Manager.Instance.CurrentJumpsBeforeNextLevel < 1) 
-			{
-				Level1Manager.Instance.LevelUp();
-			}
-//			else Level1Manager.Idle = true;
-		}
-	}
+        //		Debug.Log(string.Format("go:{0}, target: {1}, t/15: {2}", go.transform.parent.name, target, target/15f));
+
+        for (var i = 0; i < (int)(angle / RotationSpeed); i++)
+        {
+            transform.RotateAround(RotationPoint.position, Vector3.Cross(transform.up, target), RotationSpeed);// вескторное произведение чтобы определить направление поворота
+            yield return new WaitForSeconds(1f / 30f); // частота кадров анимации поворота
+        }
+        if (FrogAnimator != null) FrogAnimator.Play("Jump");
+        for (var i = 0; i < 15; i++) // количество кадров анимации прыжка
+        {
+            transform.Translate(target / Mathf.Pow(2.005f, i + 1f), Space.World);
+            yield return new WaitForSeconds(1f / 30f); // частота кадров анимации прыжка
+        }
+        transform.SetParent(go.transform);
+        transform.localPosition = Vector3.zero;
+        if (!LeadingFrog)
+        {
+            if (Level1Manager.Instance.LevelDownText.gameObject.activeSelf)
+            {
+                Level1Manager.Instance.LevelDownText.gameObject.SetActive(false);
+                yield break;
+            }
+            if (Level1Manager.NextTurn != null) Level1Manager.NextTurn();
+        }
+        else
+        {
+            if (Level1Manager.Instance.CurrentJumpsBeforeNextLevel < 1)
+            {
+                Level1Manager.Instance.LevelUp();
+            }
+            //			else Level1Manager.Idle = true;
+        }
+    }
 }
---
+++ FTF_UnityProject/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    void Awake()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+     public void Go () {
+        Time.timeScale = 1.0f;
+        gameObject.SetActive(false);
+    }
+}
